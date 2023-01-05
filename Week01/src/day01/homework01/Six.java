c1ed42a71cce1583a169ae0b99f4bf8695efa298
Default <shalpha_2@naver.com>
2023-01-05 23:51:13 +0900

    make diamod * by One 'for'

--- Week01/src/day01/homework01/Six.java
+++ Week01/src/day01/homework01/Six.java
@@ -1,44 +1,29 @@
 package day01.homework01;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Six {
 	public static void main(String[] args) {
 
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print("홀수를 입력하세요 : ");
-//		int input = sc.nextInt();
-//		System.out.println(input);
-		int input = 5;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("홀수를 입력하세요 : ");
+		int input = sc.nextInt();
 
-		for (int i = 0; i < input; i++) {
-			
+		for (int i = 1; i <= input; i++) {
+
 			// 중앙 기준 위쪽 영역
-			if (i <= input / 2) {		// i = 0, 1, 2	input = 5
-				
-				
-				
-				
-				
-				
-				//				if (condition) {
-//					
-//				}else if (condition) {
-//					
-//				}else {
-//					
-//				}
-			} 
-			
-			
-			// 중앙 기준 아래쪽 영역			i = 3, 4
-//			else {				
-//				System.out.print("#");
-//
-//			}
-			System.out.println();	// 줄바꿈
+			if (i - 1 <= input / 2) { 
+				System.out.print(" ".repeat(input / 2 - i + 1));
+				System.out.print("*".repeat(i * 2 - 1));
+			}
+
+			// 중앙 기준 아래쪽 영역 
+			else {
+				System.out.print(" ".repeat(i - input / 2 - 1));
+				System.out.print("*".repeat((input-i)*2 + 1));
+			}
+			System.out.println(); // 줄바꿈
 		}
-		
-		
+
 	}
 }
